018402eb283ca09c553d2ab6f48c5f73bdf9e78e
Sumito Izumita <sumito@izumita.com>
2020-07-17 14:31:43 +0900

    rewrite

--- cogs/tts.py
+++ cogs/tts.py
@@ -4,6 +4,17 @@ import re
 import emoji_data_python
 
 
+class TTSRequest:
+    def __init__(self, text, message, lang, voice, speed, pitch):
+        self.text = text
+        self.guild = message.guild
+        self.message = message
+        self.lang = lang
+        self.voice = voice
+        self.speed = speed
+        self.pitch = pitch
+
+
 message_dict = {
     'MongoDB': 'もんごDB',
     'wavenet': 'ウェーブネット',
@@ -34,8 +45,17 @@ class TTS(commands.Cog):
         lang = 'ja'
         if re.match("([a-zA-Z]{2})::(.+)", text):
             match = re.match("([a-zA-Z]{2})::(.+)", text)
-            lang = match.groups()[0]
+            lang = match.groups()[0] if match.groups()[0] in ['en'] else 'ja'
             text = match.groups()[1]
+
+        text = await self.make_text(text, message)
+        setting = await self.bot.db.get_user_setting(message.author.id)
+        await self.bot.voice_hooks[message.channel.id].put(
+            TTSRequest(text, message, lang, setting['voice'], setting['speed'], setting['pitch'])
+        )
+
+    async def make_text(self, text, message):
+        guild = message.guild
         text = re.sub(r"https?://[\w!?/+\-_~;.,*&@#$%()'[\]]+", "URL省略、", text)
         for emoji_name in re.findall(r"<:(.+):[0-9]+>", text):
             if self.bot.guild_setting[message.guild.id]['emoji']:
@@ -56,10 +76,27 @@ class TTS(commands.Cog):
         text = re.sub(r"^([w]+)$", "わら", text)
         text = re.sub(r"([^a-zA-Z])([w]+)$", r"\1、わら", text)
 
+        # bot辞書
         for key, value in message_dict.items():
             text = text.replace(key, value)
 
-        await self.bot.voice_hooks[message.channel.id].put([str(message.author.id).encode(), text, message.author, lang])
+        # 名前
+        if self.bot.voice_hooks[message.channel.id].before_user_id != message.author.id:
+            name = message.author.nick or message.author.name
+            if self.bot.guild_setting[guild.id]['name']:
+                text = name + "、" + text
+
+        # ユーザー辞書
+        d = await self.bot.db.get_user_dict(guild.id)
+        del d['__id']
+        for key, value in d.items():
+            text = text.replace(key, value)
+
+        # 以下略
+        if len(text) > self.bot.guild_setting[guild.id]['limit']:
+            text = text[:self.bot.guild_setting[guild.id]['limit']] + '、以下略'
+
+        return text
 
 
 def setup(bot):
--- cogs/utils/request.py
+++ cogs/utils/request.py
@@ -1,35 +1,33 @@
-import aiohttp
 import json
-import os
 import base64
 url = "https://texttospeech.googleapis.com/v1/text:synthesize"
 
 
-async def request_tts(session, text, speed, pitch, lang, voice):
+async def request_tts(access_token, session, req):
     headers = {
-        "Authorization": "Bearer " + os.environ.get('tts'),
+        "Authorization": "Bearer " + access_token[:-1],
         "Content-Type": "application/json",
         "charset": "utf-8"
     }
     body = {
         "input": {
-            "text": text
+            "text": req.text
         },
         "voice": {
-            "languageCode": lang,
-            "name": voice,
+            "languageCode": convert_lang(req.lang),
+            "name": convert_voice(req.lang, req.voice),
         },
         "audioConfig": {
             "audioEncoding": "LINEAR16",
-            "speakingRate": speed,
+            "speakingRate": req.speed,
             "sampleRateHertz": 48000,
-            "pitch": pitch,
+            "pitch": req.pitch,
         }
     }
 
     async with session.post(url, json=body, headers=headers) as r:
-        res = json.loads(await r.text())
-
+        text = await r.text()
+    res = json.loads(text)
     content = base64.b64decode(res["audioContent"])
     return remove_header(content)
 
@@ -38,4 +36,16 @@ def remove_header(content):
     return content[44:]
 
 
+def convert_lang(lang):
+    if lang == "ja":
+        return "ja-JP"
+    else:
+        return "en-US"
+
+
+def convert_voice(lang, voice):
+    if lang == "ja":
+        return f"ja-JP-Wavenet-{voice}"
+    else:
+        return f"en-US-Wavenet-{voice}"
 
